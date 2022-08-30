9fda1e67fa85e8f059484e7590bb18d1cd7fb9ac
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2022-08-30 20:22:01 +0400

    test

---
+++ core/main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomeListView.as_view(), name='home'),
+    path('about/', views.AboutListView.as_view(), name='about'),
+    path('contact/', views.ContactListView.as_view(), name='contact'),
+    path('gallery/', views.GalleryListView.as_view(), name='gallery'),
+    path('products/', views.ProductsListView.as_view(), name='products'),
+    path('products/<str:id>', views.CarCategListView.as_view(), name='car_categ'),
+    path('services/', views.ServicesListView.as_view(), name='services'),
+
+]
---
+++ core/main/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .forms import GeeksForm
+from .models import HomeSlider, Banner, CarModel, Social, HomeOnecolumn, Callus, HomeTwocolumns, HomeThreecolumns, Category, Product
+# Create your views here.
+
+class HomeListView(ListView):
+    template_name = 'index.html'
+
+    def get(self, request):
+        context = GeeksForm()
+        banner = Banner.objects.all()
+        onecolumn = HomeOnecolumn.objects.all()
+        twocolumns = HomeTwocolumns.objects.all()
+        threecolumns = HomeThreecolumns.objects.all()
+        social = Social.objects.all()
+        slider = HomeSlider.objects.all()
+        return render(request, self.template_name, {'slider':slider, "banner":banner, "context":context, "social":social, "onecolumn":onecolumn, "twocolumns":twocolumns, "threecolumns":threecolumns})
+
+class AboutListView(ListView):
+    template_name = 'about.html'
+
+    def get(self, request):
+        banner = Banner.objects.all()
+        slider = HomeSlider.objects.all()
+        return render(request, self.template_name, {'slider':slider, "banner":banner})
+
+class ContactListView(ListView):
+    template_name = 'contact.html'
+
+    def get(self, request):
+        banner = Banner.objects.all()
+        slider = HomeSlider.objects.all()
+        return render(request, self.template_name, {'slider':slider, "banner":banner})
+
+
+class GalleryListView(ListView):
+    template_name = 'gallery.html'
+
+    def get(self, request):
+        banner = Banner.objects.all()
+        slider = HomeSlider.objects.all()
+        return render(request, self.template_name, {'slider':slider, "banner":banner})
+
+
+class ProductsListView(ListView):
+    template_name = 'products.html'
+
+    def get(self, request):
+        banner = Banner.objects.all()
+        callus = Callus.objects.all()
+        slider = HomeSlider.objects.all()
+        # category = Category.objects.all()
+        category = Category.objects.all()
+        # carmodel = CarModel.objects.all()
+        return render(request, self.template_name, {'slider':slider, "banner":banner, "category":category, "callus":callus })
+
+class CarCategListView(ListView):
+    template_name = 'cars.html'
+
+    def get(self, request, id):
+        cars = Category.objects.filter(pk=id)
+        return render(request, self.template_name, {'cars':cars})
+
+class ServicesListView(ListView):
+    template_name = 'services.html'
+
+    def get(self, request):
+        banner = Banner.objects.all()
+        slider = HomeSlider.objects.all()
+        return render(request, self.template_name, {'slider':slider, "banner":banner})
